1d729fed762277d58c91dfac4cfeff50d377a2c6
Ahsaan <ahsaan@Ahsaans-Macbook-Air.local>
2023-10-31 11:38:10 +0530

    corrected env variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 export const config = {
-  firebaseAPIKey : String(import.meta.env.FIREBASE_API_KEY),
-  authDomain : String(import.meta.env.FIREBASE_AUTH_DOMAIN),
-  projectId : String(import.meta.env.FIREBASE_PROJECT_ID),
-  storageBucket : String(import.meta.env.FIREBASE_STORAGE_BUCKET),
-  senderId : String(import.meta.env.FIREBASE_MESSAGE_SENDER_ID),
-  appid : String(import.meta.env.FIREBASE_APP_ID),
+  firebaseAPIKey : String(import.meta.env.VITE_FIREBASE_API_KEY),
+  authDomain : String(import.meta.env.VITE_FIREBASE_AUTH_DOMAIN),
+  projectId : String(import.meta.env.VITE_FIREBASE_PROJECT_ID),
+  storageBucket : String(import.meta.env.VITE_FIREBASE_STORAGE_BUCKET),
+  senderId : String(import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID),
+  appid : String(import.meta.env.VITE_FIREBASE_APP_ID),
 };
