fac3fd62ed4ad0f122c4562c2eeafbb283b2148b
Ahsaan <ahsaan@Ahsaans-Macbook-Air.local>
2023-10-31 11:22:35 +0530

    added env variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,8 @@
 export const config = {
-  projectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-  apiEndpoint: String(import.meta.env.VITE_APPWRITE_API_ENDPOINT),
-  databaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-  collectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-  bucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+  firebaseAPIKey : String(import.meta.env.FIREBASE_API_KEY),
+  authDomain : String(import.meta.env.FIREBASE_AUTH_DOMAIN),
+  projectId : String(import.meta.env.FIREBASE_PROJECT_ID),
+  storageBucket : String(import.meta.env.FIREBASE_STORAGE_BUCKET),
+  senderId : String(import.meta.env.FIREBASE_MESSAGE_SENDER_ID),
+  appid : String(import.meta.env.FIREBASE_APP_ID),
 };
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { config } from "../config/config";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDNxFiFtcSdSG48mMKLbfftFtMEz_jUWZo",
-  authDomain: "react-project-9b931.firebaseapp.com",
-  projectId: "react-project-9b931",
-  storageBucket: "react-project-9b931.appspot.com",
-  messagingSenderId: "988635924609",
-  appId: "1:988635924609:web:2bac0f0fa4074e7e756580"
+  apiKey: config.firebaseAPIKey,
+  authDomain: config.authDomain,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.senderId,
+  appId: config.appid
 };
 
 // Initialize Firebase
